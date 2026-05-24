f04daae176966d1f4fcf9d35fc224b101e5f3e03
VN-78 <vishwanath.s.god@gmail.com>
2026-05-25 03:49:10 +0530

    added the transition and header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from '$lib/components/organisms/Header.svelte';
+	import { fly } from 'svelte/transition';
+	import { quartOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
+
+	let isMounted = $state(false);
+
+	onMount(() => {
+		isMounted = true;
+	});
+</script>
+
+<!-- Change header accordingly to project  -->
+<svelte:head>
+	<title>Svelte Template</title>
+
+	<meta
+		name="description"
+		content="SvelteKit template used for quick Scafolding of new projects."
+	/>
+
+	<meta property="og:title" content="svelte-template" />
+	<meta
+		property="og:description"
+		content="svelte-template is a SvelteKit template used for quick Scafolding of new projects."
+	/>
+	<meta property="og:type" content="website" />
+</svelte:head>
+
+{#if isMounted}
+	<div in:fly={{ y: -40, duration: 600, easing: quartOut }}>
+		<Header />
+	</div>
+{/if}
